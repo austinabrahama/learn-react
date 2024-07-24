b718a8ed2b25ea30138c3029139fda26c33c3d74
austin-9903 <austin.a@zohocorp.com>
2024-07-24 23:58:04 +0530

    React modules included

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-const heading = React.createElement("div", {}, [
-    React.createElement("div", {}, [React.createElement("h1", {}, "I am h1 tag"), React.createElement("h2", {}, "I am h2 tag")]), React.createElement("div", {}, [React.createElement("h1", {}, "I am h1 tag"), React.createElement("h2", {}, "I am h2 tag")])
-]);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Heading = () => <h1>React using JSX</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<Heading />);
