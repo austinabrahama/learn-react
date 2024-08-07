473777ef335316d0baa53cea97ac11ed18001613
austin-9903 <austin.a@zohocorp.com>
2024-08-08 00:20:13 +0530

    UseState implemented

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,23 @@
 import Header from "./Header";
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+    let [list, setList] = useState(resList);
+    console.log('setList: ', setList);
     return (
         <div>
             <Header />
+
+            <div className="filter">
+                <button className="filter-btn" onClick={() => {
+                    let res = resList.filter(item => item.info.avgRatingString > 4.5 );
+                    setList(res);
+                }}>Filter Restaurant</button>
+            </div>
             <div className="resCardContainer">
-                {resList.map((resData) => (
+                {list.map((resData) => (
                     <RestaurantCard key={resData.info.id} resData={resData} />
                 ))}
             </div>
